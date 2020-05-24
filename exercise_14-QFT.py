13eb3bed8b48f99976004bf7cc588a818de59309
Floris van den Ende <floreman@Floriss-MacBook-Pro.local>
2020-05-24 16:06:31 +0200

    did QFT for real

--- exercise_14-QFT.py
+++ exercise_14-QFT.py
@@ -2,14 +2,21 @@ import cirq
 
 def QFT(qubits):
     circuit = cirq.Circuit()
+    # circuit.append(cirq.H(qubits[0]))
+    # circuit.append(cirq.CZ(qubits[0], qubits[1])**0.5)
+    # circuit.append(cirq.CZ(qubits[0], qubits[2])**0.25)
+    # circuit.append(cirq.H(qubits[1]))
+    # circuit.append(cirq.CZ(qubits[1], qubits[2])**0.5)
+    # circuit.append(cirq.H(qubits[2]))
+    # circuit.append(cirq.SWAP(qubits[0], qubits[2]))
     n = len(qubits)
-    circuit.append(cirq.H(qubits[0]))
-    circuit.append(cirq.CZ(qubits[0], qubits[1])**0.5)
-    circuit.append(cirq.CZ(qubits[0], qubits[2])**0.25)
-    circuit.append(cirq.H(qubits[1]))
-    circuit.append(cirq.CZ(qubits[1], qubits[2])**0.5)
-    circuit.append(cirq.H(qubits[2]))
-    circuit.append(cirq.SWAP(qubits[0], qubits[2]))
+    for qubit in qubits:
+        circuit.append(cirq.H(qubit))
+        i = 1
+        for target in qubits[(qubits).index(qubit)+1:]:
+            circuit.append(cirq.CZ(qubit, target)**(0.5**i))
+            i += 1
+    circuit.append(cirq.SWAP(qubits[0], qubits[n-1]))
     return circuit
 
 if __name__ == "__main__":
@@ -17,7 +24,7 @@ if __name__ == "__main__":
     np.set_printoptions(linewidth = 180)
     qubits = [cirq.GridQubit(0,i) for i in range(3)]
     circuit = QFT(qubits)
-    matrix = circuit.to_unitary_matrix(qubit_order = list(circuit.all_qubits().difference(qubits)) + qubits)[:8,:8]
+    matrix = circuit.unitary(qubit_order = list(circuit.all_qubits().difference(qubits)) + qubits)[:8,:8]
     expected_matrix = np.exp(.25j*np.pi * (np.arange(8)[:,np.newaxis] * np.arange(8)[np.newaxis,:])) / np.sqrt(8)
     inner_product = np.sum(np.conj(expected_matrix.flatten()) * matrix.flatten()) / 8
     corrected_matrix = np.exp(-1.j * np.angle(inner_product)) * matrix
